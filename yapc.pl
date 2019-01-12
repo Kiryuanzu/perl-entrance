952f0d1d8c55e65698b090e45fcf1fd033abd621
kiryuanzu <anzu.nagato.1005@gmail.com>
2019-01-12 23:22:09 +0900

    first commit

---
+++ yapc.pl
@@ -0,0 +1,11 @@
+#おまじない
+use strict;
+use warnings;
+
+use DateTime;
+my $dt = DateTime->now( time_zone=>'local' );
+
+print "家.pm\n";
+print "$dt\n";
+print "YAPC:Tokyo\n";
+print "Hello, world\n";
